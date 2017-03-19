929381d0dbd8433a9bd9215f9707f2bb6caced77
Isidromgl <isidromgl@gmail.com>
2017-03-19 19:10:54 +0100

    updated js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var json;
 $.getJSON('js/estudios.json',function(data){
   json = data;
   console.log('Recuperamos datos');
+  console.log(data);
 $.each(json["education"], function(i, ed){
 $("table.table").append("<tr><td>" + ed.company + "</td><td>" + ed.position + "</td><td>" + ed.location + "</td><td>" + ed.period + "</td></tr>");
 });
@@ -14,6 +15,7 @@ var js;
 $.getJSON('js/comp.json',function(data){
   js= data;
   console.log('Recuperamos datos');
+  console.log(data);
 $.each(js["complementary"], function(i, com){
 $("table.table2").append("<tr><td>" + com.company + "</td><td>" + com.position + "</td><td>" + com.location + "</td><td>" + com.time + "</td><td>" + com.period + "</td></tr>");
 });

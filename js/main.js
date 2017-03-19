a11654c270362f3664aa7c6f6e5f13ce5fe7043d
Isidromgl <isidromgl@gmail.com>
2017-03-19 19:06:55 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -10,3 +10,11 @@ $.each(json["education"], function(i, ed){
 $("table.table").append("<tr><td>" + ed.company + "</td><td>" + ed.position + "</td><td>" + ed.location + "</td><td>" + ed.period + "</td></tr>");
 });
 });
+var js;
+$.getJSON('js/comp.json',function(data){
+  js= data;
+  console.log('Recuperamos datos');
+$.each(js["complementary"], function(i, com){
+$("table.table2").append("<tr><td>" + com.company + "</td><td>" + com.position + "</td><td>" + com.location + "</td><td>" + com.time + "</td><td>" + com.period + "</td></tr>");
+});
+});
